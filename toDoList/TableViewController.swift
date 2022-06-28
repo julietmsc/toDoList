4a9cf66dba868998434143d1e918206c8ad68709
Juliet Scott <holyokeirish@gmail.com>
2022-06-28 11:52:43 -0400

    added hello world for this to work

--- toDoList/TableViewController.swift
+++ toDoList/TableViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-
 class TableViewController: UITableViewController {
 
     var toDos : [toDo] = []
@@ -62,7 +61,7 @@ class TableViewController: UITableViewController {
         return cell
     }
    
-
+print("hello world")
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
